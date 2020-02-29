85bace1ad42566320a78e0f61cc143e0a76ebdf4
Samir Saadi <samirsaadi@gmail.com>
2020-02-29 16:58:09 -0300

    Added create ingredient feature

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i9&6jyah5g**d@@2^%jwxrs5#g7)r-mipiox$v5w@_f+-&9*7d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
